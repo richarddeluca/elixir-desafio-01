0ba677ed2d6a19124ddf92c2fea916a33da9be24
Richard Deluca <richard.deluca.dev@gmail.com>
2022-10-31 17:14:19 -0300

    changed to recursive

--- lib/list_length.ex
+++ lib/list_length.ex
@@ -1,5 +1,11 @@
 defmodule ListLength do
-  def call(list) do
-    Enum.count(list)
+
+  def call(list), do: count(list, 0)
+
+  defp count([], acc), do: acc
+
+  defp count([_head | tail], acc) do
+    acc = acc + 1
+    count(tail, acc)
   end
 end
